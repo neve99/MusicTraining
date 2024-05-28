2d465a2101c41176191a2a4a94c4c0258a439d8d
neve99 <165163178+neve99@users.noreply.github.com>
2024-05-28 18:05:38 +0200

    add wrong button

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,19 @@ function playSound(soundId) {
   sound.currentTime = 0; // Rewind to the start
   sound.play();
 }
+
+//Wrong button
+document.addEventListener('DOMContentLoaded', function () {
+  // Select all buttons with the class 'btn-wrong'
+  const buttons = document.querySelectorAll('.btn-wrong');
+
+  // Add a click event listener to each button
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      button.style.backgroundColor = 'red';
+      button.style.color = 'white';
+      button.style.border = 'none';
+      button.style.padding = '2px 5px';
+    });
+  });
+});
